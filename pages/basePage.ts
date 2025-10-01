1c3f6f73bae1a282c2e70d54d292eeaf8695f143
thotska <thotska@gmail.com>
2025-09-30 20:26:51 -0500

    VerifySubcriptionTest

--- pages/basePage.ts
+++ pages/basePage.ts
@@ -1,17 +1,37 @@
-import {Locator, Page} from "@playwright/test"
+import { Locator, Page, expect} from "@playwright/test"
 
 
 export class BasePage {
-     page: Page 
+    page: Page
     private topNavigationBarLocators: Locator
+    private footerText: Locator
+    private emailAddressField: Locator
+    private arrowButton: Locator
+    private successMessage: Locator
 
-    constructor(page: Page){
+    constructor(page: Page) {
         this.page = page
         this.topNavigationBarLocators = page.locator('ul[class="nav navbar-nav"] li')
-
+        this.footerText = page.getByRole('heading', { name: 'Subscription' })
+        this.emailAddressField = page.getByRole('textbox', { name: 'Your email address' })
+        this.arrowButton = page.getByRole('button', { name: '' })
+        this.successMessage = page.getByText('You have been successfully subscribed!')
     }
 
-    async clickOnNavigationLink(linkText: string){
+    async verifyFooterText(): Promise<void> {
+        await expect(this.footerText).toBeVisible()
+        await expect(this.footerText).toHaveText('Subscription')
+    }
+    async enterEmailAndClickArrow(email: string): Promise<void> {
+        await this.emailAddressField.fill(email)
+        await this.arrowButton.click()
+    }
+
+    async verifySuccessMessage(): Promise<void> {
+        await expect(this.successMessage).toBeVisible()
+        await expect(this.successMessage).toHaveText('You have been successfully subscribed!')
+    }
+    async clickOnNavigationLink(linkText: string) {
         await this.topNavigationBarLocators.getByText(linkText).click()
     }
 }
--- tests/regression/testCase10.spec.ts
+++ tests/regression/testCase10.spec.ts
@@ -1,17 +1,15 @@
 import{test} from '@playwright/test';
 import {Homepage} from '../../pages/homepage';
 import { BasePage } from '../../pages/basePage';    
-import {FooterPage} from '../../pages/footerPage';
 
 
 test.describe('Footer section validation', async () => {
     let homePage: Homepage   
-    let footerPage: FooterPage
     let basePage: BasePage
 
     test.beforeEach(async ({ page }) => {
         homePage = new Homepage(page)
-        footerPage = new FooterPage(page)
+    
         basePage = new BasePage(page)
     })
 
@@ -19,9 +17,9 @@ test.describe('Footer section validation', async () => {
         await page.goto('https://automationexercise.com/')
         await homePage.verifyHomePage()
         await homePage.scrollToBottom()
-        await footerPage.verifyFooterText()
-        await footerPage.enterEmailAndClickArrow('test@example.com')
-        await footerPage.verifySuccessMessage()
+        await basePage.verifyFooterText()
+        await basePage.enterEmailAndClickArrow('test@example.com')
+        await basePage.verifySuccessMessage()
     })
 
 })
