1e9d05bd549be5317f9637737652df28e5065783
thotska <thotska@gmail.com>
2025-10-03 10:50:28 -0500

    first commit

--- pages/productsPage.ts
+++ pages/productsPage.ts
@@ -7,6 +7,8 @@ export class ProductsPage extends BasePage {
     private allProductsHeading: Locator
     private searchField: Locator
     private searchButton: Locator
+    private pageHeading: Locator
+    private viewProductLinkLocators: Locator
   
 
     constructor(page: Page){
@@ -14,6 +16,8 @@ export class ProductsPage extends BasePage {
         this.allProductsHeading = page.getByRole('heading', { name: 'All Products' })
         this.searchField = page.getByRole('textbox', { name: 'Search Product' })
         this.searchButton = page.getByRole('button', { name: '' })
+        this.pageHeading = page.locator("h2[class='title text-center']")
+        this.viewProductLinkLocators = page.locator("div[class='choose'] ul[class='nav nav-pills nav-justified']")
     }
 
     async verifyAllProductsPage():Promise<void>{
@@ -26,4 +30,11 @@ export class ProductsPage extends BasePage {
         await this.searchButton.click()
 
     }
+    async verifyCommonProductsHeading(text: string):Promise<void>{
+        await this.pageHeading.isVisible()
+        expect (this.pageHeading).toHaveText(text)
+    }
+    async clickOnViewProduct(number: number):Promise<void>{
+        await this.viewProductLinkLocators.nth(number).click()
+    }   
 }
